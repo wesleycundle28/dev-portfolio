5529c0c34cf3d91754a666cebb4fabf549cee677
wesley cundle <wesley.adam.cundle@hotmail.com>
2022-03-05 19:24:28 -0500

    added anchor tag

--- src/pages/MyProjects/MyProjects.jsx
+++ src/pages/MyProjects/MyProjects.jsx
@@ -1,5 +1,15 @@
 import "./MyProjects.css";
 
 export const MyProjects = () => {
-  return <h1>My Projects Page</h1>;
+  return (
+    <div>
+      <h1>My Projects Page</h1>
+      <a
+        href="https://mern-task-list-application.herokuapp.com/"
+        target={"_blank"}
+      >
+        Task Application
+      </a>
+    </div>
+  );
 };
