cdb814dcbf5ce36b5e06b7ce059af8dde3749f2b
wesley cundle <wesley.adam.cundle@hotmail.com>
2022-03-06 21:39:42 -0500

    text added

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,6 +4,24 @@ export const About = () => {
   return (
     <div className="about" id="about">
       <h1>About Page</h1>
+      <h2>Welcome to my portfolio.</h2>
+      <h3>Intro</h3>
+      <p className="statement">
+        Hello my name is Wesley Cundle. I am a 31 year old self-taught developer
+        looking to break into the field. I am passionate about learning and
+        greatly enjoy the ever evolving world of web development. My journey
+        started with a udemy course on HTML, CSS, and JavaScript. From there I
+        have progressed to React js, redux and RTK Query. I also have experience
+        developing back end code using Node js and express with a mongo
+        database.
+      </p>
+      <h3>Projects</h3>
+      <p className="statement">
+        Below you will see my projects that I have developed. My very first
+        project taught me a great deal and gave me a strong foundation for MERN
+        stack applications. This application also has a simple json web token
+        authentication for user login.
+      </p>
     </div>
   );
 };
--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -4,6 +4,17 @@ export const Skills = () => {
   return (
     <div id="skills" className="skills">
       <h1>Skills Page</h1>
+      <ul className="skills-list">
+        <li>JavaScript</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>React js</li>
+        <li>RTK Query</li>
+        <li>Node js</li>
+        <li>express</li>
+        <li>mongoose</li>
+        <li>mongoDB</li>
+      </ul>
     </div>
   );
 };
