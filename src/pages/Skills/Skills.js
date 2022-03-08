af877866714b92b8ebca0dd2efe483d86acf377c
wesley cundle <wesley.adam.cundle@hotmail.com>
2022-03-08 16:20:45 -0500

    icons added

--- src/pages/Skills/Skills.js
+++ src/pages/Skills/Skills.js
@@ -1,4 +1,17 @@
 import "./Skills.css";
+import { FaNode, FaReact } from "react-icons/fa";
+import {
+  SiRedux,
+  SiExpress,
+  SiCss3,
+  SiHtml5,
+  SiJava,
+  SiJavascript,
+  SiMongodb,
+  SiAzuredevops,
+  SiCsharp,
+} from "react-icons/si";
+import { DiJavascript, DiMsqlServer } from "react-icons/di";
 
 export const Skills = () => {
   return (
@@ -6,15 +19,43 @@ export const Skills = () => {
       <div className="skills-container">
         <h1>Skills</h1>
         <ul className="skills-list">
-          <li className="skills-item">JavaScript</li>
-          <li className="skills-item">HTML</li>
-          <li className="skills-item">CSS</li>
-          <li className="skills-item">React js</li>
-          <li className="skills-item">RTK Query</li>
-          <li className="skills-item">Node js</li>
-          <li className="skills-item">express</li>
-          <li className="skills-item">mongoose</li>
-          <li className="skills-item">mongoDB</li>
+          <li className="skills-item">
+            <SiJavascript /> JavaScript
+          </li>
+          <li className="skills-item">
+            <SiHtml5 /> HTML
+          </li>
+          <li className="skills-item">
+            <SiCss3 /> CSS
+          </li>
+          <li className="skills-item">
+            <FaReact /> React js
+          </li>
+          <li className="skills-item">
+            <SiRedux /> Redux
+          </li>
+          <li className="skills-item">
+            <SiRedux /> RTK Query
+          </li>
+          <li className="skills-item">
+            <FaNode /> Node js
+          </li>
+          <li className="skills-item">
+            <SiExpress /> Express js
+          </li>
+          {/* <li className="skills-item"> Mongoose </li> */}
+          <li className="skills-item">
+            <SiMongodb /> MongoDB
+          </li>
+          <li className="skills-item">
+            <DiMsqlServer /> SQL
+          </li>
+          <li className="skills-item">
+            <SiAzuredevops /> Azure devops
+          </li>
+          <li className="skills-item">
+            <SiCsharp /> C#
+          </li>
         </ul>
       </div>
     </div>
