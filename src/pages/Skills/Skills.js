d5704957f185910a4adcd1914ae5db5a411bf350
wesley cundle <wesley.adam.cundle@hotmail.com>
2022-03-08 16:39:52 -0500

    testing prod issue

--- src/pages/Skills/Skills.js
+++ src/pages/Skills/Skills.js
@@ -1,15 +1,15 @@
 import "./Skills.css";
-import { FaNode, FaReact } from "react-icons/fa";
-import {
-  SiRedux,
-  SiExpress,
-  SiCss3,
-  SiHtml5,
-  SiJavascript,
-  SiMongodb,
-  SiAzuredevops,
-  SiCsharp,
-} from "react-icons/si";
+// import { FaNode, FaReact } from "react-icons/fa";
+// import {
+//   SiRedux,
+//   SiExpress,
+//   SiCss3,
+//   SiHtml5,
+//   SiJavascript,
+//   SiMongodb,
+//   SiAzuredevops,
+//   SiCsharp,
+// } from "react-icons/si";
 
 export const Skills = () => {
   return (
@@ -17,34 +17,16 @@ export const Skills = () => {
       <div className="skills-container">
         <h1>Skills</h1>
         <ul className="skills-list">
-          <li className="skills-item">
-            <SiJavascript /> JavaScript
-          </li>
-          <li className="skills-item">
-            <SiHtml5 /> HTML
-          </li>
-          <li className="skills-item">
-            <SiCss3 /> CSS
-          </li>
-          <li className="skills-item">
-            <FaReact /> React js
-          </li>
-          <li className="skills-item">
-            <SiRedux /> Redux
-          </li>
-          <li className="skills-item">
-            <SiRedux /> RTK Query
-          </li>
-          <li className="skills-item">
-            <FaNode /> Node js
-          </li>
-          <li className="skills-item">
-            <SiExpress /> Express js
-          </li>
+          <li className="skills-item">JavaScript</li>
+          <li className="skills-item">HTML</li>
+          <li className="skills-item">CSS</li>
+          <li className="skills-item">React js</li>
+          <li className="skills-item">Redux</li>
+          <li className="skills-item">RTK Query</li>
+          <li className="skills-item">Node js</li>
+          <li className="skills-item">Express js</li>
           {/* <li className="skills-item"> Mongoose </li> */}
-          <li className="skills-item">
-            <SiMongodb /> MongoDB
-          </li>
+          <li className="skills-item">MongoDB</li>
           {/* <li className="skills-item">
             <DiMsqlServer /> SQL
           </li>
