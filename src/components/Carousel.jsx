121c4623e8f4a612200ee39e6e6363e802536100
wesley cundle <wesley.adam.cundle@hotmail.com>
2022-03-07 20:28:23 -0500

    carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
+import { IoArrowUndo, IoArrowRedo } from "react-icons/io5";
 import { Images } from "../helpers/CarouselData";
 import {
   CarouselContainer,
   CarouselInnerContainer,
+  CarouselLeft,
+  CarouselRight,
+  CarouselCenter,
 } from "../styles/Carousel.style";
 
 export const Carousel = () => {
@@ -12,7 +16,23 @@ export const Carousel = () => {
       <CarouselContainer>
         <CarouselInnerContainer
           style={{ backgroundImage: `url(${Images[curr].image})` }}
-        ></CarouselInnerContainer>
+        >
+          <CarouselLeft
+            onClick={() => {
+              curr > 0 && setCurr(curr - 1);
+            }}
+          >
+            <IoArrowUndo />
+          </CarouselLeft>
+          <CarouselCenter></CarouselCenter>
+          <CarouselRight
+            onClick={() => {
+              curr < Images.length - 1 && setCurr(curr + 1);
+            }}
+          >
+            <IoArrowRedo />
+          </CarouselRight>
+        </CarouselInnerContainer>
       </CarouselContainer>
     </>
   );
--- src/styles/Carousel.style.js
+++ src/styles/Carousel.style.js
@@ -8,10 +8,45 @@ export const CarouselContainer = styled.div`
 `;
 
 export const CarouselInnerContainer = styled.div`
+  display: flex;
   width: 100%;
   height: 100%;
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
-  /* background-image: url(images.img); */
+`;
+
+export const CarouselLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 5%;
+  font-size: xx-large;
+  height: 100%;
+  background: rgb(0, 0, 0, 0.4);
+
+  :hover {
+    color: blueviolet;
+  }
+`;
+
+export const CarouselCenter = styled.div`
+  flex: 80%;
+  height: 100%;
+`;
+
+export const CarouselRight = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 5%;
+  font-size: xx-large;
+  height: 100%;
+  background: rgb(0, 0, 0, 0.4);
+
+  :hover {
+    color: blueviolet;
+  }
 `;
